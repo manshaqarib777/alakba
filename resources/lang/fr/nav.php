d4d21eaa3a0497060ee08f2d6d9f53d2a1416144
mansha.qarib777@gmail.com <mansha.qarib777@gmail.com>
2021-05-03 10:21:10 +0500

    first push

---
+++ resources/lang/fr/nav.php
@@ -0,0 +1,85 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'dashboard' => 'Tableau de Bord',
+    'admin' => 'Admin',
+    'announcements' => 'Annonces',
+    'users' => 'Utilisateurs',
+    'customers' => 'Clients',
+    'currencies' => 'Devises',
+    'shops' => 'Boutiques',
+    'shipping_zones' => 'Zones de Livraison',
+    'suppliers' => 'Fournisseurs',
+    'vendors' => 'Vendeurs',
+    'merchants' => 'Marchands',
+    'manufacturers' => 'Fabricants',
+    'blogs' => 'Blogs',
+    'settings' => 'Paramètres',
+    'catalog' => 'Catalogue',
+    'categories' => 'Catégories',
+    'groups' => 'Groupes',
+    'sub-groups' => 'Sous-Groupes',
+    'products' => 'Produits',
+    'attributes' => 'Attributs',
+    'stock' => 'Stock',
+    'inventories' => 'Inventaires',
+    'warehouses' => 'Entrepôts',
+
+    'utilities' => 'Utilitaires',
+    'support' => 'Assistance',
+    'support_messages' => 'Messages',
+    'disputes' => 'Litiges',
+    'support_tickets' => 'Tickets',
+    'refunds' => 'Remboursements',
+    'user_roles' => 'Les Rôles',
+    'taxes' => 'Taxes',
+    'themes' => 'Thèmes',
+    'theme_options' => 'Options du Thème',
+    'banners' => 'Banners',
+    'sliders' => 'Sliders',
+    'shipping' => 'Livraison',
+    'packaging' => 'Emballage',
+    'carriers' => 'Transporteurs',
+    'payment_methods' => 'Modes de Paiement',
+    'payment_statuses' => 'Statuts de Paiement',
+    'email_templates' => 'Modèles d\'emails',
+    'config' => 'Configuration',
+    'general' => 'Info Générale',
+    'orders' => 'Commandes',
+    'carts' => 'Paniers',
+    'invoices' => 'Factures',
+    'payments' => 'Paiements',
+    'promotions' => 'Promotions',
+    'coupons' => 'Coupons',
+    'gift_cards' => 'Cartes Cadeaux',
+    'faqs' => 'FAQS',
+    'pages' => 'Pages',
+    'appearance' => 'Apparence',
+    'performance' => 'Performance',
+
+    'reports' => 'Rapports',
+    'visitors' => 'Visiteurs',
+    'sales_report' => 'Ventes',
+    'system_settings' => 'Paramètres',
+    'subscription_plans' => 'Plans',
+
+    // Version 1.4.0
+    'chats' => 'chats',
+    'countries' => 'Zone Business',
+
+    // Version 2.1.0
+    'cancellations' => 'Annulations',
+
+];
