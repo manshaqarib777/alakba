7e897ce3656f6a0c745dfe245eb590fd27b17353
mansha.qarib777@gmail.com <mansha.qarib777@gmail.com>
2021-09-04 10:50:43 +0500

    asasa

--- app/Providers/ViewComposerServiceProvider.php
+++ app/Providers/ViewComposerServiceProvider.php
@@ -540,9 +540,9 @@ class ViewComposerServiceProvider extends ServiceProvider
                 $view->with('current_plan', Auth::user()->getCurrentPlan());
                 $view->with('billable', Auth::user()->shop->hasPaymentMethod() ? Auth::user()->shop : Null);
 
-                if(SystemConfig::isPaymentConfigured('stripe') && Auth::user()->shop->createSetupIntent()) {
-                    $view->with('intent', Auth::user()->shop->createSetupIntent());
-                }
+                // if(SystemConfig::isPaymentConfigured('stripe')) {
+                //     $view->with('intent', Auth::user()->shop->createSetupIntent());
+                // }
             }
         );
     }
